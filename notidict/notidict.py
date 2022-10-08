1548968134d23477715821632beb1c902bafbf15
pixelhegel <pixelhegel@gmail.com>
2022-10-08 21:28:08 +0800

    Removed xsel, changed usage way

--- notidict/notidict.py
+++ notidict/notidict.py
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
+
 """
 NotiDict
 NotiDict is a terminal dictionary application, you can use it to query the words from your own .mdx files,and the result will be shown by system notification. And it also supports uploading the result to Notion. You also can use it to upload highlight text to Notion. The best practice is to bind a shortcut to the app, then you can easily query the dict and highlight text anyevery.
 
 Usage:
- notidict -d | --dict
- notidict -l | --highlight
+ notidict dict <word>
+ notidict highlight <text>
  notidict -h | --help
 
 Options:
-  --dict  query the selected word
-  --highlight  send the selected sentence to notion
+ --help -h
 """
 import os
 import json
@@ -24,11 +25,13 @@ from pyquery import PyQuery as pq
 from readmdict import MDD, MDX
 import yaml
 from docopt import docopt
+import pyclip
 
 #from notidict import __version__
 
 def join(f):
     return os.path.join(os.path.dirname(__file__), f)
+
 
 stream = open(join("config.yml"), 'r')
 dictionary = yaml.safe_load(stream)
@@ -36,6 +39,7 @@ filename = dictionary['mdx_dict_path']
 notion_vocabulary_database = dictionary['notion_vocabulary_database']
 notion_highlight_database = dictionary['notion_highlight_database']
 NOTION_API_KEY = os.getenv('NOTION_API_KEY')
+
 
 def sendmessage(title, message):
     subprocess.Popen(['notify-send', title, message])
@@ -88,50 +92,51 @@ def send_newword_to_notion(word, source, date, database):
         'https://api.notion.com/v1/pages', headers=headers, json=json_data)
     return response
 
-def create_new_page_with_conetent(title,content,date,database):
+
+def create_new_page_with_conetent(title, content, date, database):
     url = "https://api.notion.com/v1/pages/"
     payload = json.dumps({
-    "parent": {
-        "database_id": database
-    },
-    "properties": {
-        "Name": {
-        "title": [
-            {
-            "text": {
-                "content": title
-            }
-            }
-        ]
+        "parent": {
+            "database_id": database
         },
-        "Updated": {
-        "date": {
-            "start": date,
-            "end": None
-        }
-        }
-    },
-    "children": [
-        {
-        "object": "block",
-        "type": "paragraph",
-        "paragraph": {
-            "rich_text": [
-            {
-                "type": "text",
-                "text": {
-                "content": content
+        "properties": {
+            "Name": {
+                "title": [
+                    {
+                        "text": {
+                            "content": title
+                        }
+                    }
+                ]
+            },
+            "Updated": {
+                "date": {
+                    "start": date,
+                    "end": None
                 }
             }
-            ]
-        }
-        }
-    ]
+        },
+        "children": [
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": content
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
     })
     headers = {
-    'Content-Type': 'application/json',
-    'Notion-Version': '2022-06-28',
-    'Authorization': f"Bearer {NOTION_API_KEY}",
+        'Content-Type': 'application/json',
+        'Notion-Version': '2022-06-28',
+        'Authorization': f"Bearer {NOTION_API_KEY}",
 
     }
 
@@ -160,38 +165,43 @@ def query_page_by_title(title, database):
     return response.json()['results']
 
 
-def update_highlight_to_page(title,content,database):
+def update_highlight_to_page(title, content, database):
     url = "https://api.notion.com/v1/blocks/{0}/children".format(database)
     payload = json.dumps({
-    "children": [
-        {
-        "object": "block",
-        "type": "paragraph",
-        "paragraph": {
-            "rich_text": [
+        "children": [
             {
-                "type": "text",
-                "text": {
-                "content": content
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": content
+                            }
+                        }
+                    ]
                 }
             }
-            ]
-        }
-        }
-    ]
+        ]
     })
+
     headers = {
-    'Authorization':  f"Bearer {NOTION_API_KEY}",
-    'Content-Type': 'application/json',
-    'Notion-Version': '2022-06-28'
+        'Authorization':  f"Bearer {NOTION_API_KEY}",
+        'Content-Type': 'application/json',
+        'Notion-Version': '2022-06-28'
     }
 
     response = requests.request("PATCH", url, headers=headers, data=payload)
 
 
-def get_selected_text():
-    var = os.popen('xsel').read()
+def get_selected_text(args):
+    if args['dict']:
+        var = args['<word>']
+    else:
+        var = args['<text>']
     return var
+
 
 def get_application_title():
     gi.require_version("Wnck", "3.0")
@@ -199,46 +209,52 @@ def get_application_title():
     scr = Wnck.Screen.get_default()
     scr.force_update()
     return scr.get_active_window().get_name()
- 
 
-def query_dict():
+
+def query_dict(args):
     source = get_application_title()
-    var = get_selected_text()
+    content = get_selected_text(args)
+    if len(content) > 3:
+        headwords = [*MDX(filename)]
+        items = [*MDX(filename).items()]
+        queryword = content.lower().strip().replace(',', '').replace('.', '')
 
-    headwords = [*MDX(filename)]
-    items = [*MDX(filename).items()]
-    queryword = var.lower().strip().replace(',', '').replace('.', '')
+        word_index = headwords.index(queryword.encode())
+        word, html = items[word_index]
+        word, html = word.decode(), html.decode()
 
-    word_index = headwords.index(queryword.encode())
-    word, html = items[word_index]
-    word, html = word.decode(), html.decode()
+        soup = BeautifulSoup(html)
 
-    soup = BeautifulSoup(html)
-
-    sendmessage(word, str(soup))
-    today = date.today().strftime("%Y-%m-%d")
-    respone = send_newword_to_notion(
-        word, source, today, notion_vocabulary_database)
+        sendmessage(word, str(soup))
+        today = date.today().strftime("%Y-%m-%d")
+        respone = send_newword_to_notion(
+            word, source, today, notion_vocabulary_database)
 
 
-def update_highlight():
+def update_highlight(args):
+    content = get_selected_text(args)
+    if len(content) < 3:
+        content = pyclip.paste()
+        content = content.decode('utf-8')
+
     source = get_application_title()
-    content = get_selected_text()
-    result = query_page_by_title(source,notion_highlight_database)
-    if len(result)>0:
+    result = query_page_by_title(source, notion_highlight_database)
+
+    if len(result) > 0:
         page_id = result[0]['id']
-        update_highlight_to_page(source,content,page_id)
+        update_highlight_to_page(source, content, page_id)
     else:
         today = date.today().strftime("%Y-%m-%d")
-        respone = create_new_page_with_conetent(source, content, today, notion_highlight_database)
+        respone = create_new_page_with_conetent(
+            source, content, today, notion_highlight_database)
     sendmessage('Highlight saved to', source)
 
 
 def init(args):
-    if args["--dict"] or ["-d"]:
-        query_dict()
-    if args["--highlight"] or args["-l"]:
-        update_highlight()
+    if args["dict"]:
+        query_dict(args)
+    if args["highlight"]:
+        update_highlight(args)
 
 
 def main():
